3e6a054729f63b485c2054e5c5c0be3bb7b39978
jeffreybiles <bilesjeffrey@gmail.com>
2017-05-07 16:56:40 -0500

    update host name in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tutorial_site, TutorialSite.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "immense-everglades-49956.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "www.phoenix-tutorial.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
