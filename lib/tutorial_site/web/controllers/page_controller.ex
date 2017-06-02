b0151b48ded4488f72e73a6ff1c73ce485d5fa65
jeffreybiles <bilesjeffrey@gmail.com>
2017-06-02 00:59:09 -0500

    Custom page titles

--- lib/tutorial_site/web/controllers/page_controller.ex
+++ lib/tutorial_site/web/controllers/page_controller.ex
@@ -6,23 +6,25 @@ defmodule TutorialSite.Web.PageController do
   end
 
   def read_book(conn, _params) do
-    render conn, "read_book.html", book: Book.contents
+    render conn, "read_book.html", book: Book.contents, page_title: "Read The Phoenix Tutorial online for free"
   end
 
   def read_chapter(conn, %{"section" => section, "chapter" => chapter}) do
-    render conn, "read_chapter.html", section: section, chapter: chapter, book: Book.contents
+    readable_chapter = "#{chapter} (#{section}, The Phoenix Tutorial)" |> String.split("-") |> Enum.join(" ")
+    render conn, "read_chapter.html", section: section, chapter: chapter,
+                  book: Book.contents, page_title: readable_chapter
   end
 
   def buy_ebook(conn, _params) do
-    render conn, "buy_ebook.html"
+    render conn, "buy_ebook.html", page_title: "Ebook for The Phoenix Tutorial- Coming Soon!"
   end
 
   def buy_videos(conn, _params) do
-    render conn, "buy_videos.html"
+    render conn, "buy_videos.html", page_title: "Screencasts for The Phoenix Tutorial- Coming Soon!"
   end
 
   def mailing_list(conn, _params) do
-    render conn, "mailing_list.html"
+    render conn, "mailing_list.html", page_title: "Mailing List signup for The Phoenix Tutorial"
   end
 
   def contact(conn, _params) do
