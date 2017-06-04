5cbf19e166d26bd84267c6b92ee29f95c45a879c
jeffreybiles <bilesjeffrey@gmail.com>
2017-06-04 14:05:57 -0500

    remove "buy" from url names, since we want those for the post

--- lib/tutorial_site/web/controllers/payment_controller.ex
+++ lib/tutorial_site/web/controllers/payment_controller.ex
@@ -1,11 +1,11 @@
 defmodule TutorialSite.Web.PaymentController do
   use TutorialSite.Web, :controller
 
-  def buy_ebook(conn, _params) do
-    render conn, "buy_ebook.html", page_title: "Ebook for The Phoenix Tutorial", phoenix_testimonials: Testimonials.phoenix
+  def ebook(conn, _params) do
+    render conn, "ebook.html", page_title: "Ebook for The Phoenix Tutorial", phoenix_testimonials: Testimonials.phoenix
   end
 
-  def buy_videos(conn, _params) do
-    render conn, "buy_videos.html", page_title: "Screencasts for The Phoenix Tutorial- Coming Soon!"
+  def videos(conn, _params) do
+    render conn, "videos.html", page_title: "Screencasts for The Phoenix Tutorial- Coming Soon!"
   end
 end
--- lib/tutorial_site/web/router.ex
+++ lib/tutorial_site/web/router.ex
@@ -19,8 +19,8 @@ defmodule TutorialSite.Web.Router do
     get "/", PageController, :index
     get "/read", PageController, :read_book
     get "/read/:section/:chapter", PageController, :read_chapter
-    get "/buy_ebook", PaymentController, :buy_ebook
-    get "/buy_videos", PaymentController, :buy_videos    
+    get "/ebook", PaymentController, :ebook
+    get "/videos", PaymentController, :videos
     get "/mailing_list", PageController, :mailing_list
     get "/contact", PageController, :contact
   end
